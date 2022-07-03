55e44f1ae71f2cbbe6392f1d48cfaec3327bb7e6
hopejieshuo <101780409+hopejieshuo@users.noreply.github.com>
2022-07-03 12:44:00 +0800

    Update script.js

--- docs/PC/script.js
+++ docs/PC/script.js
@@ -1,12 +1,3 @@
 document.querySelector(".one").onclick=function(){
     window.open("001 Clock/index.html");
 }
-document.querySelector(".two").onclick=function(){
-    window.open("002 Battery/index.html");
-}
-document.querySelector(".three").onclick=function(){
-    window.open("003 RandNum/index.html");
-}
-document.querySelector(".four").onclick=function(){
-    window.open("004 Search/index.html");
-}
