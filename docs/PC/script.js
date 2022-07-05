02b7fe33e905daea3e67e381a40f6c9e243e98a0
hopejieshuo <101780409+hopejieshuo@users.noreply.github.com>
2022-07-05 09:13:53 +0800

    Update script.js

--- docs/PC/script.js
+++ docs/PC/script.js
@@ -1,3 +1,6 @@
 document.querySelector(".one").onclick=function(){
     window.open("001 Clock/index.html");
 }
+document.querySelector(".two").onclick=function(){
+    window.open("002 Battery/index.html");
+}
